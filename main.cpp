cf26d4a32ffb381b2c374cae16d0cb4fcba3bc82
Sashwat K <sashwat0001@gmail.com>
2019-08-10 17:39:05 +0530

    added variables

--- main.cpp
+++ main.cpp
@@ -3,3 +3,26 @@
 #include <string.h>
 #include "sgfplib.h"
 
+LPSGFPM  sgfplib = NULL;
+
+int main(int argc, char **argv) {
+    long err;
+    DWORD templateSize, templateSizeMax;
+    char kbBuffer[100];
+    int fingerLength = 0;
+    char *finger;
+
+    BYTE *imageBuffer1;
+    BYTE *minutiaeBuffer1;
+
+    FILE *fp = NULL;
+
+    SGFingerInfo fingerInfo;
+    SGDeviceInfoParam deviceInfo;
+
+    DWORD timeout = 10000;
+    DWORD quality = 85;
+    DWORD quality_of_image = 0;
+
+    
+}
