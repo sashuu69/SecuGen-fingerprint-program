a7fc7608efd8cc5e97f6e96ff1c82d8ba2999a49
Sashwat K <sashwat0001@gmail.com>
2019-08-27 21:30:53 +0530

    working on c code

--- fingerprint-matching/main.cpp
+++ fingerprint-matching/main.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "sgfplib.h"
+
+#include <mysql/mysql.h>
+
+static char *host = "docker01.tharun.me";
+static char *user = "docker";
+static char *pass = "docker";
+static char *dbname = "exam-marker";
 
 
 int main(int argc, char* argv[]) {
     char* studentRegistrationNumber = argv[1];
-    printf("%s",studentRegistrationNumber);
+    // printf("%s",studentRegistrationNumber);
 }
