896cd2f938a45710ae97539f453b399a4d1f3ac3
Sashwat K <sashwat0001@gmail.com>
2019-08-27 23:38:24 +0530

    working on secugen authentication

--- fingerprint-matching/main.cpp
+++ fingerprint-matching/main.cpp
@@ -18,14 +18,24 @@ unsigned int flag = 0;
 
 int main(int argc, char* argv[]) {
     MYSQL *conn;
-    char* studentRegistrationNumber = argv[1];
+    char *studentRegistrationNumber = argv[1];
+    char *sqlSelectQuery = "SELECT `fingerprint` FROM `students_details` WHERE `university_id` = '";
+    char *sqlQueryEnder = "'";
+
+    int finalQuerySize = strlen(sqlSelectQuery) + strlen(studentRegistrationNumber) + strlen(sqlQueryEnder) + 1;
+    char *finalQuery = (char *)malloc(finalQuerySize);
+    strcpy(finalQuery,sqlSelectQuery);
+    strcat(finalQuery, studentRegistrationNumber);
+    strcat(finalQuery,sqlQueryEnder);
 
     conn = mysql_init(NULL);
     if(!(mysql_real_connect(conn, host, user, pass, dbname, port, unix_socket, flag))) {
-        fprintf(stderr, "\nError: %s [%d]\n",mysql_error(conn), mysql_errno(conn));
+        // fprintf(stderr, "\nError: %s [%d]\n",mysql_error(conn), mysql_errno(conn));
         exit(1);
     }
-    printf("success");
+
+    printf("%s",finalQuery);
+
+    
     return EXIT_SUCCESS;
-    // printf("%s",studentRegistrationNumber);
 }
