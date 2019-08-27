bd8e9cf4c8e11a807105a44a0dd09590fe9f7330
Sashwat K <sashwat0001@gmail.com>
2019-08-27 21:43:43 +0530

    sql code on c worked finally

--- fingerprint-matching/main.cpp
+++ fingerprint-matching/main.cpp
@@ -11,8 +11,21 @@ static char *user = "docker";
 static char *pass = "docker";
 static char *dbname = "exam-marker";
 
+unsigned int port = 3306;
+static char *unix_socket = NULL;
+unsigned int flag = 0;
+
 
 int main(int argc, char* argv[]) {
+    MYSQL *conn;
     char* studentRegistrationNumber = argv[1];
+
+    conn = mysql_init(NULL);
+    if(!(mysql_real_connect(conn, host, user, pass, dbname, port, unix_socket, flag))) {
+        fprintf(stderr, "\nError: %s [%d]\n",mysql_error(conn), mysql_errno(conn));
+        exit(1);
+    }
+    printf("success");
+    return EXIT_SUCCESS;
     // printf("%s",studentRegistrationNumber);
 }
